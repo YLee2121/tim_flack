6f4c2c5001f533f5f374f35837af6d0aff23e5bd
YiLee <kyleleey@bu.edu>
2022-10-08 11:23:44 -0400

    edit post function

--- websites/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteNote(product_id) {
-    fetch('/profile/delete/', {
-      method: "POST",
-      body: JSON.stringify({ product_id: product_id }),
-    }).then((_res) => {
-      window.location.href = "/";
-    });
-  }
--- websites/user.py
+++ websites/user.py
@@ -56,9 +56,41 @@ def add_product():
 
     return render_template('add_product.html')
 
-@user.route('/profile/update/')
-def update():
-    return "update product"
+@user.route('/profile/edit/<product_id>', methods=['POST', 'GET'])
+def edit(product_id):
+
+    # user object id to edit 
+    object_id = ObjectId(product_id)
+    p = db.product.find_one({"_id":object_id})
+
+    if request.method == "POST":
+        # get the request post
+        title = request.form.get('product_title')
+        category = request.form.get('product_category')
+        price = request.form.get('product_price')
+        description = request.form.get('product_description')
+        owner = session['user_email']
+
+        # deifine a product 
+        p_edit = {
+            "owner":owner,
+            "title":title, 
+            "category":category, 
+            "price":price, 
+            "description":description
+        }
+
+        # update db
+        db.product.update_one ({"_id":object_id}, {"$set": p_edit}, upsert=False)
+
+        # redirect to profile
+        flash('Product Edited!', category='success')
+
+        return redirect(url_for('user.profile'))
+
+
+
+    return render_template("edit_product.html", product = p)
 
 @user.route('/profile/delete/<product_id>')
 def delete(product_id):
