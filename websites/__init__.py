370ae31f137a214fe92b6fe14177ac58c55a8628
YiLee <kyleleey@bu.edu>
2022-10-14 15:30:23 -0400

    upload pic, delete pic, show the post pic

--- websites/__init__.py
+++ websites/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from pymongo import MongoClient
 
 # global parameter
-UPLOAD_FOLDER = './websites/static/product_pic/'
-
+SRC_TO_PIC_PATH = 'static/product_pic/'
+APP_TO_PIC_PATH = './websites/static/product_pic/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 MONGO_URI = "mongodb+srv://ylee21:0000@cluster0.bnf6h1k.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
 db = client.BU_MarketPlace_DB
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def create_app():
@@ -16,7 +20,6 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "secret_key"
     app.config['MONGO_URI'] = MONGO_URI
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     # register the blueprint
     from .views import views 
--- websites/user.py
+++ websites/user.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, render_template, redirect, url_for, session, request, flash, jsonify
-from . import db, UPLOAD_FOLDER
+from . import db, APP_TO_PIC_PATH, SRC_TO_PIC_PATH, allowed_file
 from bson.objectid import ObjectId
 from datetime import datetime
 from werkzeug.utils import secure_filename
@@ -30,53 +30,64 @@ def profile():
 @user.route('/profile/add_product', methods=["POST", "GET"])
 def add_product():
 
-    if request.method == "POST":
+    if request.method != "POST":
+        return render_template('add_product.html')
         
-        # get the request
-        title = request.form.get('product_title')
-        category = request.form.get('product_category')
-        price = request.form.get('product_price')
-        description = request.form.get('product_description')
-        owner = session['user_email']
-        post_date = datetime.today()
-        
-        
-        
-        # save pic
-        pic_name = secure_filename(request.files['product_pic'].filename)
-        product_pic = request.files['product_pic']
+    # get the request
+    title = request.form.get('product_title')
+    category = request.form.get('product_category')
+    price = request.form.get('product_price')
+    description = request.form.get('product_description')
+    owner = session['user_email']
+    post_date = datetime.now()
+            
+            
 
-        # check dir
-        personal_dir = UPLOAD_FOLDER + session['user_email'] + '/'
-        if not os.path.exists(personal_dir):
-            os.mkdir(personal_dir)
-        path = personal_dir + pic_name 
+    ##### pic processing
+    pic_name = secure_filename(request.files['product_pic'].filename)
+    product_pic = request.files['product_pic']
 
-        # save pic 
-        product_pic.save(path)
+    # check extension 
+    if not allowed_file(pic_name):
+        flash('Image format not allowed!', category='error')
+        return render_template('add_product.html')
+
+    # check dir app.py viewpoint
 
 
+    personal_dir = APP_TO_PIC_PATH + session['user_email'] + '/'
+    if not os.path.exists(personal_dir):
+        os.mkdir(personal_dir)
+
+    save_path = personal_dir + pic_name
+    path_for_html = f'../{SRC_TO_PIC_PATH}' + session['user_email'] + '/' + pic_name
 
 
-        # define a product 
-        p = {
-            "owner":owner,
-            "title":title, 
-            "category":category, 
-            "price":price, 
-            "description":description,
-            "post_date": post_date
-        }
+    # save pic 
+    product_pic.save(save_path)
 
-        # update db
-        db.product.insert_one(p)
 
-        # redirect to profile
-        flash('Product Add!', category='success')
 
-        return redirect(url_for('user.profile'))
 
-    return render_template('add_product.html')
+    # define a product 
+    p = {
+        "owner":owner,
+        "title":title, 
+        "category":category, 
+        "price":price, 
+        "description":description,
+        "post_date": post_date,
+        "pic_path_for_html": path_for_html,  # save in user.py viewpoint
+        "pic_path_for_app": save_path
+    }
+
+    # update db
+    db.product.insert_one(p)
+
+    # redirect to profile
+    flash('Product Add!', category='success')
+
+    return redirect(url_for('user.profile'))
 
 
 
@@ -124,7 +135,17 @@ def delete(product_id):
 
     # use object id to remove one product from db.product
     object_id = ObjectId(product_id)
+    # delete the pic
+    p = db.product.find_one({"_id":object_id})
+    try:
+        os.remove(p['pic_path_for_app'])
+        
+    except:
+        pass 
+
+    # delete the db
     db.product.delete_one({"_id":object_id})
+    
     flash('product delete!', category='success')
 
     return redirect(url_for('user.profile'))
